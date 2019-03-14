9b46de4f15ca7c9b7e8b732883c74f1bad4f0a8f
marcus lin <marcuslin0724@gmail.com>
2019-03-14 21:32:20 +0800

    Create sessions_controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+    redirect_to edit_user_path(session[:user]) if current_user
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user && user.authenticate(params[:password])
+      session[:user] = user.id
+      redirect_to edit_user_path(user.id), notice: "Welcome back! #{ user.name }"
+    else
+      flash[:error] = ["Invalid Email or Password"]
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user] = ''
+    redirect_to root_path, notice: "Bye!!"
+  end
+end
