cb3721236a846172d86dcef0492d2e5ba1805f78
marcus lin <marcuslin0724@gmail.com>
2019-03-14 21:39:08 +0800

    Add reset_password_sent_at to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190314073141) do
+ActiveRecord::Schema.define(version: 20190314122400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20190314073141) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "confirmed_at"
+    t.datetime "reset_password_sent_at"
   end
 
 end
