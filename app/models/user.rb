4267a76a924af7c08d334daa9f88300eb02196bd
marcus lin <marcuslin0724@gmail.com>
2019-03-14 23:15:23 +0800

    Update password validate, add has_secure_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :password, presence: true, length: { minimum: 8 }, allow_nil: true
+
+  has_secure_token :reset_password_token
 
   validates :email, presence: true
   validates :email, uniqueness: true
   validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 
-  validates :password, length: { minimum: 8 }
 end
