bc60a04e7e399920c1de6ebcbcec9c28b77c382d
marcus lin <marcuslin0724@gmail.com>
2019-03-14 18:21:15 +0800

    Add email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 end
