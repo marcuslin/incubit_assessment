864acca42cca7e88c9d0abcae36fdc4312f42ab7
marcus lin <marcuslin0724@gmail.com>
2019-03-15 01:29:22 +0800

    Add reset_password edit and update to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'sessions#new'
   resources :sessions
-  resources :reset_passwords
+  resources :reset_passwords, only: [:new, :create, :edit, :update], param: :token
   resources :users
 end
