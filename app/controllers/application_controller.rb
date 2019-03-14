f4bcb506f0b12fcd13987f157ffd9ed34346c877
marcus lin <marcuslin0724@gmail.com>
2019-03-14 22:36:54 +0800

    Set current_user as helper_method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user
 
   def current_user
-    User.find_by_id(session[:user])
+    if session[:user]
+      @current_user = User.find_by_id(session[:user])
+    else
+      @current_user = nil
+    end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,2 @@
 module ApplicationHelper
-  def current_user
-    User.find_by_id(session[:user])
-  end
 end
