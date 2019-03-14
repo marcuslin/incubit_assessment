3258f9ccba8066e127549b9ae9d280ee76153765
marcus lin <marcuslin0724@gmail.com>
2019-03-14 23:11:51 +0800

    Add reset_password to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'sessions#new'
   resources :sessions
+  resources :reset_passwords
   resources :users
 end
